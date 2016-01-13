e526c4c128ff8e63dfa82bd42cb478db239d3aef
Peter Jang <peter.x.jang@gmail.com>
2016-01-13 12:30:49 -0600

    added join table for meetings and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113174503) do
+ActiveRecord::Schema.define(version: 20160113181614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meeting_users", force: :cascade do |t|
+    t.integer  "meeting_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meetings", force: :cascade do |t|
     t.datetime "start_time"
