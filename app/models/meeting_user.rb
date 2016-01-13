77c634a826ae28d390e87e97a4834069221fe451
Peter Jang <peter.x.jang@gmail.com>
2016-01-13 12:35:18 -0600

    added associations between user and meeting

--- app/models/meeting_user.rb
+++ app/models/meeting_user.rb
@@ -1,2 +1,4 @@
 class MeetingUser < ActiveRecord::Base
+  belongs_to :meeting
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :meeting_users
+  has_many :meetings, through: :meeting_users
 end
