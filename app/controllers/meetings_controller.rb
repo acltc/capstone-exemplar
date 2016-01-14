a7de0fa8fd31bb21bea2ea53d6089840a0e631ff
Peter Jang <peter.x.jang@gmail.com>
2016-01-13 20:26:45 -0600

    added meeting new action (with associated model) and show action

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,4 +4,30 @@ class MeetingsController < ApplicationController
   def index
     @meetings = current_user.meetings
   end
+
+  def new
+    @meeting = Meeting.new
+    @members = User.where('id != ?', current_user.id)
+  end
+
+  def create
+    @meeting = Meeting.create(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time]
+    )
+    member_ids = params[:members] + [current_user.id]
+    member_ids.each do |member_id|
+      MeetingUser.create(
+        meeting_id: @meeting.id,
+        user_id: member_id
+      )
+    end
+    redirect_to "/meetings/#{@meeting.id}"
+  end
+
+  def show
+    @meeting = Meeting.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/' => 'landing_page#index'
   get '/meetings' => 'meetings#index'
+  get '/meetings/new' => 'meetings#new'
+  post '/meetings' => 'meetings#create'
+  get '/meetings/:id' => 'meetings#show'
 end
