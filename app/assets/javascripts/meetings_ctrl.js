9081cd542008feec4d556f1b494561e2fd393f75
Peter Jang <peter.x.jang@gmail.com>
2016-01-18 12:33:29 -0600

    added calendar config and sample event data

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -3,7 +3,39 @@
   angular.module('app').controller('meetingsCtrl', function($scope, $http) {
     $scope.setup = function() {
       $scope.message = "yoyoyo";
-      $scope.eventSources = [];
+      $scope.uiConfig = {
+        calendar:{
+          height: 450,
+          editable: true,
+          header:{
+            left: 'month basicWeek basicDay agendaWeek agendaDay',
+            center: 'title',
+            right: 'today prev,next'
+          },
+          defaultView: 'agendaWeek',
+          dayClick: $scope.alertEventOnClick,
+          eventDrop: $scope.alertOnDrop,
+          eventResize: $scope.alertOnResize
+        }
+      };
+      $scope.eventSources = {
+        events: [
+          {
+            title: 'Event1',
+            start: '2016-01-18'
+          },
+          {
+            title: 'Event2',
+            start: '2016-01-19'
+          },
+          {
+            title: 'Event3',
+            start: new Date()
+          }
+        ],
+        color: 'yellow',   // an option!
+        textColor: 'black' // an option!
+      };
       $http.get('/api/v1/meetings.json').then(function(response) {
         $scope.meetings = response.data;
       });
