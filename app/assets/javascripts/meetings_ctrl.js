85fdce6be1d0d06aa8f727b546426c92a3b50c32
Peter Jang <peter.x.jang@gmail.com>
2016-01-18 12:14:35 -0600

    implemented angular in meeting index page

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,6 +1,20 @@
 /* global angular */
 (function() {
-  angular.module('app').controller('meetingsCtrl', function($scope) {
-    $scope.message = "yoyoyo";
+  angular.module('app').controller('meetingsCtrl', function($scope, $http) {
+    $scope.setup = function() {
+      $scope.message = "yoyoyo";
+      $http.get('/api/v1/meetings.json').then(function(response) {
+        $scope.meetings = response.data;
+
+
+        // Initialize Masonry
+        var $box = $('.blog-masonry').masonry();
+        // Layout Masonry again after all images have loaded
+        $box.imagesLoaded( function() {
+          $box.masonry();
+        });
+
+      });
+    };
   });
 })();
